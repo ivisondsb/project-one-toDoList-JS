a5b895c9f50782b527bfc56ffec461db42f1fa8d
Ivisondsb <Ivisonk0@gmail.com>
2023-10-23 22:48:24 -0300

    exibir menu

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function exibirMenu() {
+  console.log("\n======= Menu =======");
+  console.log("1. Adicionar uma tarefa");
+  console.log("2. Editar uma tarefa");
+  console.log("3. Remover uma tarefa");
+  console.log("4. Listar todas as tarefas");
+  console.log("5. Obter uma tarefa por ID");
+  console.log("6. Sair");
+}
+
+exibirMenu();
