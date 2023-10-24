cccac83cde8a90199525c4b9d6a61b11e8b083f2
Edson Suraty <edinhosuraty@gmail.com>
2023-10-24 00:05:03 -0300

    criei a base do menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-function exibirMenu() {
+const prompt = require('prompt-sync')();
+
+function iniciarApp() {
+  while (true) {
+    const opcao = menuInicial();
+    selecionarOpcao(opcao);
+  }
+}
+
+function menuInicial() {
   console.log("\n======= Menu =======");
   console.log("1. Adicionar uma tarefa");
   console.log("2. Editar uma tarefa");
@@ -6,6 +15,34 @@ function exibirMenu() {
   console.log("4. Listar todas as tarefas");
   console.log("5. Obter uma tarefa por ID");
   console.log("6. Sair");
+  const opcao = prompt('Escolha uma das opções acima: ');
+
+  return opcao;
 }
 
-exibirMenu();
+function selecionarOpcao(op) {
+  switch (op) {
+    case '1':
+      console.log("Testando a opção 1");
+      break;
+    case '2':
+      console.log("Testando a opção 2");
+      break;
+    case '3':
+      console.log("Testando a opção 3");
+      break;
+    case '4':
+      console.log("Testando a opção 4");
+      break;
+    case '5':
+      console.log("Testando a opção 5");
+      break;
+    case '6':
+      console.log("Saindo...");
+      process.exit(0); // Saindo do programa
+    default:
+      console.log("Opção inválida");
+  }
+}
+
+iniciarApp();
