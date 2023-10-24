ed4731c3ee8c1636fbe94178566a0b276f80caa9
Edson Suraty <edinhosuraty@gmail.com>
2023-10-24 19:08:04 -0300

    Adiciona Tarefas com validação

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@ function iniciarApp() {
   while (true) {
     const opcao = menuInicial();
     if (!selecionarOpcao(opcao)) {
-      console.log("\nVolte sempre!")
+      console.log("\nPrograma encerrado!")
       break;
     }
   }
 }
 
 function querContinuar() {
-  const resposta = prompt('Deseja continuar? Digite "S" ou "N": ');
-  return resposta.toLowerCase() === 's';
+  while (true) {
+    const resposta = prompt('Deseja continuar? Digite "S" para continuar ou "N" para parar: ');
+    if (resposta.toLowerCase().trim() === 's') {
+      return true;
+    } else if (resposta.toLowerCase().trim() === 'n') {
+      return false;
+    } else {
+      console.log("Ação incorreta. Por favor, digite 'S' para continuar ou 'N' para parar.");
+    }
+  }
 }
 
 function menuInicial() {
@@ -42,14 +50,14 @@ function selecionarOpcao(op) {
       console.log("\nTestando a opção 3");
       break;
     case '4':
-      console.log("\nTestando a opção 4");
+      console.log(listaDeTarefas);
       break;
     case '5':
       console.log("\nTestando a opção 5");
       break;
     case '6':
-      console.log("\nSaindo do menu..");
-      break;
+      console.log("\nPrograma encerrado!");
+      return false;
     default:
       console.log("\nOpção inválida");
   }
@@ -63,6 +71,30 @@ function selecionarOpcao(op) {
 
 function adicionarTarefa() {
 
+  console.log("\n======= Adicionar uma tarefa =======");
+  let novaTarefa = prompt('Digite a nova tarefa: ').trim(); // trim() elimina espaços desnecessários na string
+
+  // Valida a entrada e repete caso seja inválido
+  while (!novaTarefa || novaTarefa.length === 0) {
+    console.log("\nPor favor, insira uma tarefa válida.");
+    novaTarefa = prompt('Digite a nova tarefa: ').trim();
+  }
+
+  // Valida se a tarefa adicionada já existe 
+  const tarefaJaExiste = listaDeTarefas.map(tarefa => tarefa.tarefa.toLowerCase()).includes(novaTarefa.toLowerCase());
+  if (tarefaJaExiste) {
+    console.log("\nEsta tarefa já existe na lista de tarefas.");
+  } else {
+    const tarefaFormatado = novaTarefa.charAt(0).toUpperCase() + novaTarefa.slice(1).toLowerCase(); 
+    //criando o objeto
+    const novaTarefaObjeto = {
+      id: listaDeTarefas.length + 1,
+      tarefa: tarefaFormatado
+    };
+    //adicionando o objeto na lista de tarefas
+    listaDeTarefas.push(novaTarefaObjeto);
+    console.log(`\nTarefa "${tarefaFormatado}" adicionada com sucesso com ID ${novaTarefaObjeto.id}!`); //mensagem de confirmação
+  }
 }
 
 iniciarApp();
